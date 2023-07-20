08a2b1aae369ca2fa55a27d7df5ba890f8fe60c8
Hossein AzadMaleki <74851226+HoseinAzad@users.noreply.github.com>
2023-07-20 02:34:14 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-# Apologies, the requested item is currently unavailable.
-# If you require the code, please feel free to email me at hosein.azadmaleki@gmail.com
+# Sharif Emotional Speech Database 
+#DATASET_PATH = '/kaggle/input/shemo-persian-speech-emotion-detection-database'
+DATASET_PATH = '/content/ShEMO'
+HUBERT = "facebook/hubert-base-ls960"
+DISTIL_HUBERT = "ntu-spml/distilhubert"
+LR = 1e-5
+EPOCHS = 20
+WEIGHT_DECAY = 0.005
+SCH_GAMMA = 0.1
+SCH_STEP = 10
